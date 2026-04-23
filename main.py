07b049856f433559460e2ba1bb4f49b9717a9345
Ryutaro-Matsuda <02ryu28@gmail.com>
2026-04-23 16:04:19 +0900

    add datasets

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader, ConcatDataset, Dataset
 
-##Setting Train Datasets
-
+#####Setting Train Datasets
 # CIFAR-10
 cifar_train = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 cifar_test  = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
@@ -13,3 +12,7 @@ cifar_test  = datasets.CIFAR10(root='./data', train=False, download=True, transf
 # SVHN
 svhn_train = datasets.SVHN(root='./data', split='train', download=True, transform=transform)
 svhn_test  = datasets.SVHN(root='./data', split='test', download=True, transform=transform)
+# CIFAR-100
+
+#(If Possible) ImageNet
+#####
